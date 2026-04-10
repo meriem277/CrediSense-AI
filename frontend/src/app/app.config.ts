d2cea6a48de38b18af76e2a2c55a6d6f2e82c43b
meriem277 <meriem.rehouma@esprit.tn>
2026-04-10 11:10:08 +0100

    changement du frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,8 @@
+import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay()),
-     provideHttpClient(withFetch())
+    provideHttpClient(withFetch())
   ]
 };
